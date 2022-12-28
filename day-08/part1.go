61df8efda7d6a56806603d101eff2740f98b46f3
sanchayata-jain <sanchayatajain@gmail.com>
2022-12-28 12:59:10 +0000

    day 8 part 2

--- day-08/part1.go
+++ day-08/part1.go
@@ -16,16 +16,26 @@ func main() {
 		log.Fatal(err)
 	}
 
-	visibleTrees, _ := partOne(trees)
+	visibleTrees, scenic, _ := partOne(trees)
 	fmt.Println(visibleTrees)
+	fmt.Println(scenic)
 }
 
-func partOne(trees [][]string) (int, error) {
+// func partTwo(trees [][]string) {
+// 	for i, row := range trees {
+// 		if i == 0 {
+// 			// first row so
+// 		}
+// 	}
+// }
+
+func partOne(trees [][]string) (int, int, error) {
 	rowLen := len(trees[0])
 	colLen := len(trees)
-	edgeTrees := ((rowLen*2) + (colLen*2)) - 4
-	
+	edgeTrees := ((rowLen * 2) + (colLen * 2)) - 4
+
 	var visibleTrees = 0
+	var scenicScore []int
 	for i, row := range trees {
 		if i == 0 {
 			// first row is visible so skip
@@ -36,15 +46,20 @@ func partOne(trees [][]string) (int, error) {
 			break
 		}
 		for j, tree := range row {
+			visibleLeft := false
+			visibleRight := false
+			visibleUp := false
+			visibleDown := false
+			// visibleTree := false
 			if j == 0 {
 				// first column of each row is visible so skip
 				continue
 			}
-			if j == len(row)-1 { 
+			if j == len(row)-1 {
 				// last column of each row is visible so skip to next row
 				break
 			}
-			
+
 			left := row[:j]    // everything left of current tree in the same row (not including the current tree)
 			right := row[j+1:] // everything right of current tree in the same row (not including the current tree)
 			var up []string
@@ -52,61 +67,169 @@ func partOne(trees [][]string) (int, error) {
 			for k := 0; k < i; k++ {
 				up = append(up, trees[k][j])
 			}
-	
+
 			for k := i + 1; k < len(trees); k++ {
 				down = append(down, trees[k][j])
 			}
 
-			maxLeft, err := max(left)
+			maxLeft, _, err := max(left)
 			if err != nil {
-				return 0, err
+				return 0, 0, err
 			}
-			maxRight, err := max(right)
+			maxRight, _, err := max(right)
 			if err != nil {
-				return 0, err
+				return 0, 0, err
 			}
-			maxUp, err := max(up)
+			maxUp, _, err := max(up)
 			if err != nil {
-				return 0, err
+				return 0, 0, err
 			}
-			maxDown, err := max(down)
+			maxDown, _, err := max(down)
 			if err != nil {
-				return 0, err
+				return 0, 0, err
 			}
 
 			treeInt, err := strconv.Atoi(tree)
 			if err != nil {
-				return 0, err
+				return 0, 0, err
 			}
 
-			if treeInt > maxLeft || treeInt > maxRight  || treeInt > maxUp || treeInt > maxDown {
+			// for part 1
+			if treeInt > maxLeft || treeInt > maxRight || treeInt > maxUp || treeInt > maxDown {
 				visibleTrees += 1
+				// visibleTree = true
 			}
+			// fmt.Println(treeInt, maxLeft)
+			// fmt.Println(treeInt, maxRight)
+			// fmt.Println(treeInt, maxUp)
+			// fmt.Println(treeInt, maxDown)
+
+			if treeInt > maxLeft {
+				visibleLeft = true
+			}
+			if treeInt > maxRight {
+				visibleRight = true
+			}
+			if treeInt > maxUp {
+				visibleUp = true
+			}
+			if treeInt > maxDown {
+				visibleDown = true
+			}
+
+			// fmt.Println(visibleLeft)
+			// fmt.Println(visibleRight)
+			// fmt.Println(visibleUp)
+			// fmt.Println(visibleDown)
+			// fmt.Println()
+
+			maxLeftIndex := findIndex(left, treeInt, "left")
+			maxRightIndex := findIndex(right, treeInt, "right")
+			maxUpIndex := findIndex(up, treeInt, "up")
+			maxDownIndex := findIndex(down, treeInt, "down")
+
+			leftDiff := 0
+			rightDiff := 0
+			upDiff := 0
+			downDiff := 0
+
+			//calculate scenic score for part 2
+
+			if !visibleLeft {
+				// fmt.Println(maxLeftIndex)
+				leftDiff = j - maxLeftIndex
+			} else if visibleLeft {
+				leftDiff = j
+			}
+			if !visibleRight {
+				rightDiff = maxRightIndex + 1
+			} else if visibleRight {
+				rightDiff = len(row) - 1 - j
+			}
+			if !visibleUp {
+				upDiff = i - maxUpIndex
+			} else if visibleUp {
+				upDiff = i
+			}
+			if !visibleDown {
+				downDiff = maxDownIndex + 1
+			} else if visibleDown {
+				downDiff = colLen - 1 - i
+			}
+			// fmt.Println(i, j)
+			// fmt.Println(visibleLeft)
+			// fmt.Println(leftDiff)
+			// fmt.Println(visibleRight)
+			// fmt.Println(rightDiff)
+			// fmt.Println(visibleUp)
+			// fmt.Println(upDiff)
+			// fmt.Println(visibleDown)
+			// fmt.Println(downDiff)
+			// fmt.Println()
+			scenicScore = append(scenicScore, leftDiff*rightDiff*upDiff*downDiff)
 		}
-		
+
 	}
 
-	return visibleTrees + edgeTrees, nil
+	maxScenicScore := max2(scenicScore)
+
+	return visibleTrees + edgeTrees, maxScenicScore, nil
 }
 
-// max returns the maximum number in a slice
-func max(section []string) (int, error) {
+func max2(section []int) int {
+	maxNum := section[0]
+	for i := 1; i < len(section); i++ {
+		currNum := section[i]
+		if currNum >= maxNum {
+			maxNum = currNum
+		}
+	}
+	return maxNum
+}
+
+func findIndex(section []string, treeVal int, direction string) (int) {
+	// finds index for number which is bigger than current tree in same row or same col (left, right, up, down)
+	closestIdx := 0
+	if direction == "left" || direction == "up" {
+		for i := range section {
+			otherTree, _ := strconv.Atoi(section[i])
+			if otherTree >= treeVal {
+				closestIdx = i
+			}
+		}
+		return closestIdx
+	}
+	if direction == "right" || direction == "down" {
+		for i := range section {
+			otherTree, _ := strconv.Atoi(section[i])
+			if otherTree >= treeVal {
+				return i
+			}
+		}
+	}
+	return 0
+}
+
+// max returns the maximum value and index in a slice
+func max(section []string) (int, int, error) {
 	maxNum, err := strconv.Atoi(section[0])
 	if err != nil {
-		return 0, err
+		return 0, 0, err
 	}
-	
+	maxIndex := 0
+
 	for i := 1; i < len(section); i++ {
 		currNumber, err := strconv.Atoi(section[i])
 		if err != nil {
-			return 0, err
+			return 0, 0, err
 		}
 		if currNumber > maxNum {
 			maxNum = currNumber
+			maxIndex = i
 		}
 	}
 
-	return maxNum, nil
+	return maxNum, maxIndex, nil
 }
 
 func readInputFile(filepath string) ([][]string, error) {
